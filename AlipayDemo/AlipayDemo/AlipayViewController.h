7e79db545c992fd23060f8326ae90e5dd2a38858
Bo <gebo@outlook.com>
2016-04-27 11:42:45 +0800

    test

---
+++ AlipayDemo/AlipayDemo/AlipayViewController.h
@@ -0,0 +1,13 @@
+//
+//  AlipayViewController.h
+//  AlipayDemo
+//
+//  Created by Bo on 16/4/27.
+//  Copyright © 2016年 Bo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlipayViewController : UIViewController
+
+@end
